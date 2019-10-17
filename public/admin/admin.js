8768c0c5601c466e802e8425f4d8bcb0e50acc6d
Administrator <administrator@CN-6.local>
2019-10-17 17:20:44 +0100

    js additions and few tweaks

---
+++ public/admin/admin.js
@@ -0,0 +1,15 @@
+const signout = document.getElementsByClassName("signout");
+const username = document.getElementsByClassName("username");
+const datedrop = document.getElementById("date-drop");
+const searchbtn = document.getElementById("search-button");
+const addButton = document.getElementById("addButton");
+const shiftTime = document.getElementsByClassName("shift-time");
+const shiftClient = document.getElementsByClassName("shift-client");
+const shiftLocation = document.getElementsByClassName("shift-location");
+const shifStaffNames = document.getElementsByClassName("shift-staff-names");
+const hoursWorked = document.getElementsByClassName("hours-staff-worked");
+const informationButton = document.getElementsByClassName("shift-client-details-button");
+const editButton = document.getElementsByClassName("shift-edit-shift-button");
+const deleteButton = document.getElementsByClassName("shift-delete-button");
+const formSubmitButton = document.getElementsByClassName("new-client-submit");
+
