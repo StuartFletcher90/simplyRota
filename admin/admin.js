84992cb65ee122ca4b8597ffac84abe65e2eba15
CN105 <cn105@CN0105.local>
2019-10-18 17:42:40 +0100

    adding on DOM creation of shift cards

--- admin/admin.js
+++ admin/admin.js
@@ -19,15 +19,97 @@ const closer = document.getElementById("closer");
 const addShiftFormBtn = document.getElementById("add-shift-form-btn");
 const addShiftBtn = document.getElementById("addShift-Btn");
 const newClientWrapper = document.getElementById("new-client-wrapper");
-// const fetchData = async () => {
-//     console.log("Fetching data!")
-//     let response = await fetch(/lists-shifts=${})
-//     let data = await response.json()
-//     console.log(data)
-//     // displayData(data)
-// }
-// fetchData()
-// shift-cards-wrapper
+
+const shiftCardsWrapper = document.getElementById("shift-cards-wrapper")
+
+
+// ---------- Display all shift cards ------//
+
+const displayData = (shiftData) => {
+
+  // console.log(data.data[0])
+
+  shiftCardsWrapper.innerHTML = ""  // clear every time its loaded
+
+  // for(i=0; i<shiftData.data.length; i++){
+
+  //   console.log(shiftData.data[i])
+  // }
+
+  shiftData.data.map((shiftDatesObject) => {
+
+      console.table(shiftDatesObject)
+
+      // create the div with shift content in it
+
+      let div = document.createElement("div")
+      shiftCard.className = "shift-card"
+
+      shiftCardsWrapper.appendChild(shiftCard)
+
+      let shiftClient = document.createElement("h5")
+      shiftClient.className = "shift-client"
+      shiftClientDetailsButton.innerHTML = // **** nned this
+      // Need to appendChild to shiftCard
+
+
+      let shiftTime = document.createElement("h5")
+      shiftTime.className = "shift-time"
+      shiftClientDetailsButton.innerHTML =
+
+
+      let shiftLocation = document.createElement("h5")
+      shiftLocation.className = "shift-location"
+      shiftClientDetailsButton.innerHTML =
+
+
+      let shiftClient = document.createElement("h5")
+      shiftStaffNames.className = "shift-staff-names"
+      shiftClientDetailsButton.innerHTML =
+
+
+      let shiftClient = document.createElement("h5")
+      hoursStaffWorked.className = "hours-staff-worked"
+      shiftClientDetailsButton.innerHTML =
+
+
+      let shiftClient = document.createElement("button")
+      shiftClientDetailsButton.className = "shift-client-details-button"
+      siftClientDetailsButton.setAttribute("data-shiftDetail", "")h
+      shiftClientDetailsButton.innerHTML = '<i class="fas fa-info-circle"></i>'
+
+      
+      let shiftClient = document.createElement("button")
+      shiftEditShiftButton.className = "shift-edit-shift-button"
+      shiftClientDetailsButton.innerHTML = '<i class="fas fa-edit"></i>'
+
+
+      let shiftClient = document.createElement("button")
+      shiftDeleteButton.className = "shift-delete-button"
+      shiftClientDetailsButton.innerHTML = '<i class="fas fa-trash-alt"></i>'
+
+
+  })
+
+
+}
+
+{/* <div class="shift-cards-wrapper">
+<div class="shift-card">
+    <h5 class="shift-client">Client: Miss Rogers</h5>
+    <h5 class="shift-time">Time: 9:00 - 14:00</h5>
+    <h5 class="shift-location">Location:Chester</h5>
+    <h5 class="shift-staff-names">Amanda,Lisa</h5>
+    <h5 class="hours-staff-worked">Duration:69</h5>
+    <button class="shift-client-details-button" data-shiftDetail=""><i class="fas fa-info-circle"></i></button>
+    <button class="shift-edit-shift-button"><i class="fas fa-edit"></i></button>
+    <button class="shift-delete-button"><i class="fas fa-trash-alt"></i></button>
+ */}
+
+
+
+
+
 
 
 
@@ -39,16 +121,22 @@ addShiftFormBtn.addEventListener("click", () => {
   modal.style.display = "block";
   console.log("add a shift button has been clicked")
 })
+
+
 //---------- close form on close ----------//
 closer.addEventListener("click", () => {
   modal.style.display = "none";
 })
+
+
 //---------- close form when clicked off form ----------//
 window.onclick = (event) => {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+
 //---------- add shift ----------//
 addShiftBtn.addEventListener("click", async () => {
   // only close form on submit when required inputs have been entered
@@ -62,16 +150,18 @@ addShiftBtn.addEventListener("click", async () => {
       }
 })
 
+
+
+//---------  Date Picker gets all Shifts from a date --------//
 searchbtn.addEventListener('click', async () => {
 
     let shiftDate = dateSelect.value
     console.log(`requesting shift for date of ${shiftDate}`)
     let response = await fetch(`/admin/list-shifts?shift_date=${shiftDate}`)
     let data = await response.json()
-    console.log(data)
-    // displayData(data)
+    // console.log(data)
+    displayData(data)
 })
-
 
 
 
