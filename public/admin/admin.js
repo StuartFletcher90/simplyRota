f367f99ace25f6c95a12c59edeeab536df080ace
CN74 <cn74@CN74s-MacBook-Pro.local>
2019-10-18 10:14:37 +0100

    added modal on add shift

--- public/admin/admin.js
+++ public/admin/admin.js
@@ -2,7 +2,7 @@ const signout = document.getElementsByClassName("signout");
 const username = document.getElementsByClassName("username");
 const datedrop = document.getElementById("date-drop");
 const searchbtn = document.getElementById("search-button");
-const addButton = document.getElementById("addButton");
+// const addButton = document.getElementById("addButton");
 const shiftTime = document.getElementsByClassName("shift-time");
 const shiftClient = document.getElementsByClassName("shift-client");
 const shiftLocation = document.getElementsByClassName("shift-location");
@@ -12,19 +12,41 @@ const informationButton = document.getElementsByClassName("shift-client-details-
 const editButton = document.getElementsByClassName("shift-edit-shift-button");
 const deleteButton = document.getElementsByClassName("shift-delete-button");
 const formSubmitButton = document.getElementsByClassName("new-client-submit");
+const modal = document.getElementById("add-shift-modal");
+const closer = document.getElementById("closer");
+const addShiftBtn = document.getElementById("add-shift-btn");
 
-const fetchData = async () => {
-    console.log("Fetching data!")
+// const fetchData = async () => {
+//     console.log("Fetching data!")
     
-    let response = await fetch(`/lists-shifts=${}`)
-    let data = await response.json()
+//     let response = await fetch(`/lists-shifts=${}`)
+//     let data = await response.json()
     
 
-    console.log(data)
-    // displayData(data)
+//     console.log(data)
+//     // displayData(data)
 
-}
-fetchData()
+// }
+// fetchData()
 // shift-cards-wrapper
 
 
+//---------- show add shift form ----------//
+addShiftBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+    console.log("add a shift button has been clicked")
+})
+
+
+//---------- close form on close ----------//
+closer.addEventListener("click", () => {
+    modal.style.display = "none";
+})
+
+
+//---------- close form when clicked off form ----------//
+window.onclick = (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
